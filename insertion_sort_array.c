2413079733738d4dbe3b726778dacc5b1e5a32b6
AmanHogan <100090134+AmanHogan@users.noreply.github.com>
2022-03-30 15:10:26 -0500

    Update insertion_sort_array.c

--- insertion_sort_array.c
+++ insertion_sort_array.c
@@ -9,20 +9,21 @@ void insertion_sort(int list[], int N);
 void print_array(int list[], int N);
 
 int main()
-{
+{	
 	/* 
 		Initializing an integer array
 		Storing the number of elements of array by 
 		diving the total bytes of the array by the
 		bytes of a single element 
 	*/
+	
    int arr[] = {3, 5, 1, 1, 4, 12, 7, 9};
    int N = sizeof(arr) / sizeof(arr[0]);
-	
+
 	// Printing starting array, sorting array, printing final array
-   print_array(arr, N);
-   insertion_sort(arr, N);
-   print_array(arr, N);
+   	print_array(arr, N);
+   	insertion_sort(arr, N);
+   	print_array(arr, N);
 	
 }
 
@@ -30,25 +31,26 @@ void insertion_sort(int list[], int N)
 {
    int key, i, j, temp;
 	
-	// For each element in array...
+   // For each element in array...
    for(i = 0; i < N; i++)
    {
 		// Storing the current value of array into "key"
-      key = list[i];
+      	key = list[i];
       
 		// Getting previous index
 		j = i-1;
       
-		// While the previous index is not 0, and the previous value
-		// is greater than the current value, then swap those elements
+      // While the previous index is not 0, and the previous value
+      // is greater than the current value, then swap those elements
       while((j >= 0) && (list[j] > key))
       {
-			// Swap the current element and the previous element
+		
+		 // Swap the current element and the previous element
          temp = list[j];
          list[j] = list[j+1];
          list[j+1] = temp;
          
-			// Continue to swap the sorted list elements until the index is '0'
+		// Continue to swap the sorted list elements until the index is '0'
          j--;
       }
    }
@@ -58,7 +60,7 @@ void print_array(int list[], int N)
 {
    int i;
    
-	// For each element in the array, print it to the console
+   // For each element in the array, print it to the console
    printf("Array Elements: [ ");
    for(i = 0; i < N; i++)
    {
